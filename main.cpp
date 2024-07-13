0a3aa39513639bfa2728c48240bae630bfcc74cd
dev02 <dev02@agsinn.com>
2024-07-13 09:30:25 -0300

    Atualizando a main.cpp para receber os dados de entrada apropriadamente

--- main.cpp
+++ main.cpp
@@ -1,15 +1,47 @@
-
+#include <stdio.h>
 #include "./include/Graph.hpp"
 #include "./include/utils.hpp"
 
-int main() {
-    Graph my_graph = Graph(5);
-    my_graph.insert_edge(0,2,2);
-    my_graph.insert_edge(0,4,4);
-    my_graph.insert_edge(1,2,6);
-    my_graph.insert_edge(4,2,7);
-    my_graph.insert_edge(3,4,8);
 
-    my_graph.print_graph();
+int main() {
+
+    int number_of_vertices,number_of_edges,number_of_portals;
+
+
+    std::cin>>number_of_vertices>>number_of_edges>>number_of_portals;
+
+    Graph graph = Graph(number_of_vertices);
+
+
+
+    Vertex* vertex_array = new Vertex[number_of_vertices];
+
+    for(int i=0;i<number_of_vertices;i++) {
+        int x_coordinate,y_coordinate;
+        std::cin>>x_coordinate>>y_coordinate;
+        vertex_array[i] = Vertex(i,x_coordinate,y_coordinate);
+        std::cout<<"Vertice no "<<vertex_array[i].id<<" Coordenada X:"<<vertex_array[i].x_coordinate<<std::endl;  
+    }
+
+
+    for(int i=0;i<number_of_edges;i++) {
+        int origin_vertex_id,destination_vertex_id;
+        std::cin>>origin_vertex_id>>destination_vertex_id;
+
+        graph.insert_edge(vertex_array[origin_vertex_id],vertex_array[destination_vertex_id]);
+    }
+
+    for(int i=0;i<number_of_portals;i++) {
+        int origin_vertex_id,destination_vertex_id;
+        std::cin>>origin_vertex_id>>destination_vertex_id;
+
+
+        graph.insert_portal(vertex_array[origin_vertex_id],vertex_array[destination_vertex_id]);
+
+    }
+
+
+    graph.print_graph();
     return 0;
-}
+
+}   
